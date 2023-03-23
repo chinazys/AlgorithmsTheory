903bc61f445df2de7888b1f28796f366f8b98747
chinazys <yehor.chukanov@gmail.com>
2023-03-23 21:25:17 +0200

    smooth sort upd

--- Sorting/sorters/smooth_sorter.py
+++ Sorting/sorters/smooth_sorter.py
@@ -3,36 +3,44 @@ from sorters.sorter import Sorter
 
 class SmoothSorter(Sorter):
     def _leonardo_numbers(num):
-        if num < 2:
-            return 1
-        return SmoothSorter._leonardo_numbers(num - 1) + SmoothSorter._leonardo_numbers(num - 2) + 1
-    
-    def _heapify(array, start, end):
-        i = start
-        j = 0
-        k = 0
+        a, b = 1, 1
+        numbers = []
+        while a <= num:
+            numbers.append(a)
+            a, b = b, a + b + 1
+        return numbers
 
-        while k < end - start + 1:
-            if k & 0xAAAAAAAA:
-                j = j + i
-                i = i >> 1
+    def _restore_heap(lst, i, heap, leo_nums):
+        current = len(heap) - 1
+        k = heap[current]
+
+        while current > 0:
+            j = i - leo_nums[k]
+            if (lst[j] > lst[i] and
+                (k < 2 or lst[j] > lst[i-1] and lst[j] > lst[i-2])):
+                lst[i], lst[j] = lst[j], lst[i]
+                i = j
+                current -= 1
+                k = heap[current]
             else:
-                i = i + j
-                j = j >> 1
+                break
 
-            k = k + 1
+        while k >= 2:
+            t_r, k_r, t_l, k_l = SmoothSorter._get_child_trees(i, k, leo_nums)
+            if lst[i] < lst[t_r] or lst[i] < lst[t_l]:
+                if lst[t_r] > lst[t_l]:
+                    lst[i], lst[t_r] = lst[t_r], lst[i]
+                    i, k = t_r, k_r
+                else:
+                    lst[i], lst[t_l] = lst[t_l], lst[i]
+                    i, k = t_l, k_l
+            else:
+                break
 
-        while i > 0:
-            j = j >> 1
-            k = i + j
-
-            while k < end:
-                if array[k] > array[k - i]:
-                    break
-                array[k], array[k - i] = array[k - i], array[k]
-                k = k + i
-
-            i = j
+    def _get_child_trees(i, k, leo_nums):
+        t_r, k_r = i - 1, k - 2
+        t_l, k_l = t_r - leo_nums[k_r], k - 1
+        return t_r, k_r, t_l, k_l
 
 
     def run(self):
@@ -41,29 +49,30 @@ class SmoothSorter(Sorter):
         
         time_start_ns = time.time_ns()
 
-        p = len(self.array) - 1
-        q = p
-        r = 0
-        while p > 0:
-            if (r & 0x03) == 0:
-                SmoothSorter._heapify(self.array, r, q)
-    
-            if SmoothSorter._leonardo_numbers(r) == p:
-                r = r + 1
+        leo_nums = SmoothSorter._leonardo_numbers(len(self.array))
+
+        heap = []
+
+        for i in range(len(self.array)):
+            if len(heap) >= 2 and heap[-2] == heap[-1] + 1:
+                heap.pop()
+                heap[-1] += 1
             else:
-                r = r - 1
-                q = q - SmoothSorter._leonardo_numbers(r)
-                SmoothSorter._heapify(self.array, r, q)
-                q = r - 1
-                r = r + 1
-    
-            self.array[0], self.array[p] = self.array[p], self.array[0]
-            p = p - 1
-    
-        for i in range(len(self.array) - 1):
-            j = i + 1
-            while j > 0 and self.array[j] < self.array[j - 1]:
-                self.array[j], self.array[j - 1] = self.array[j - 1], self.array[j]
-                j = j - 1
+                if len(heap) >= 1 and heap[-1] == 1:
+                    heap.append(0)
+                else:
+                    heap.append(1)
+            SmoothSorter._restore_heap(self.array, i, heap, leo_nums)
+
+        for i in reversed(range(len(self.array))):
+            if heap[-1] < 2:
+                heap.pop()
+            else:
+                k = heap.pop()
+                t_r, k_r, t_l, k_l = SmoothSorter._get_child_trees(i, k, leo_nums)
+                heap.append(k_l)
+                SmoothSorter._restore_heap(self.array, t_l, heap, leo_nums)
+                heap.append(k_r)
+                SmoothSorter._restore_heap(self.array, t_r, heap, leo_nums)
     
         self.execution_time = (time.time_ns() - time_start_ns) / 1e9
